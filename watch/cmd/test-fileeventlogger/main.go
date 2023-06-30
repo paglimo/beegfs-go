bcd929522e5fafc0f49f73d38b21118a0e01d076
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2023-06-30 13:26:43 -0500

    Add the ability to send events as quickly as possible

--- watch/cmd/test-fileeventlogger/main.go
+++ watch/cmd/test-fileeventlogger/main.go
@@ -17,7 +17,7 @@ var (
 	socketPath = flag.String("socket", "/beegfs/meta_01_tgt_0101/socket/beegfs_eventlog", "The path to the BeeGFS event log socket")
 	logFile    = flag.String("logFile", "", "log to a file instead of stdout")
 	logDebug   = flag.Bool("logDebug", false, "enable logging at the debug level")
-	frequency  = flag.Duration("frequency", time.Second, "how often an event should be sent to the socket")
+	frequency  = flag.Duration("frequency", 1*time.Second, "how often an event should be sent to the socket (0 is as fast as possible)")
 )
 
 func main() {
@@ -29,10 +29,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// We'll connect common OS signals to a context to cleanly shutdown goroutines:
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-	defer cancel()
-
 	// Setup the socket:
 	conn, err := net.Dial("unixpacket", *socketPath)
 
@@ -42,25 +38,36 @@ func main() {
 
 	defer conn.Close()
 
-	ticker := time.NewTicker(*frequency)
-	defer ticker.Stop()
 	dummyEvent := []byte{0x1, 0x0, 0x0, 0x0, 0x5d, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xb, 0x0, 0x0, 0x0, 0xc, 0x0, 0x0, 0x0, 0x30, 0x2d, 0x36, 0x34, 0x34, 0x42, 0x46, 0x46, 0x31, 0x46, 0x2d, 0x31, 0x0, 0xc, 0x0, 0x0, 0x0, 0x30, 0x2d, 0x36, 0x34, 0x34, 0x43, 0x30, 0x30, 0x31, 0x42, 0x2d, 0x31, 0x0, 0x8, 0x0, 0x0, 0x0, 0x2f, 0x62, 0x61, 0x72, 0x2f, 0x66, 0x6f, 0x6f, 0x0, 0x4, 0x0, 0x0, 0x0, 0x2f, 0x66, 0x6f, 0x6f, 0x0, 0x4, 0x0, 0x0, 0x0, 0x72, 0x6f, 0x6f, 0x74, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
 
-	//time.Sleep(1 * time.Second)
-	for {
-		select {
-		case <-ctx.Done():
-			log.Info("graceful shutdown requested")
-			return
-		case <-ticker.C:
-			//_, err := conn.WriteTo(dummyEvent, conn.LocalAddr())
+	if *frequency == 0 {
+		for {
 			_, err := conn.Write(dummyEvent)
 			if err != nil {
 				log.Fatal("error writing to socket", zap.Error(err))
 			}
 		}
-	}
+	} else {
+		// We'll connect common OS signals to a context to cleanly shutdown even if we're waiting to send an event:
+		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+		defer cancel()
 
+		ticker := time.NewTicker(*frequency)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				log.Info("graceful shutdown requested")
+				return
+			case <-ticker.C:
+				_, err := conn.Write(dummyEvent)
+				if err != nil {
+					log.Fatal("error writing to socket", zap.Error(err))
+				}
+			}
+		}
+	}
 }
 
 // getLogger parses command line logging options and returns an appropriately configured zap.Logger.
