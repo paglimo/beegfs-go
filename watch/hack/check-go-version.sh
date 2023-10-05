ec65c440ca463f271547d65f9d9b840bb1fa33a0
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2023-10-05 13:11:58 -0500

    Update Go version and run a tidy

--- watch/hack/check-go-version.sh
+++ watch/hack/check-go-version.sh
@@ -1,6 +1,6 @@
 # The GO_BUILD_VERSION is the official go version the project is built/tested with.
 # When updating be sure to also change any .github/workflows/ that install a specific Go version.
-GO_BUILD_VERSION="go1.20.6"
+GO_BUILD_VERSION="go1.20.7"
 INSTALLED_VERSION=$(go version | { read _ _ ver _; echo ${ver}; } )  || die "determining version of go failed"
 
 if [ "$INSTALLED_VERSION" == "$GO_BUILD_VERSION" ]
