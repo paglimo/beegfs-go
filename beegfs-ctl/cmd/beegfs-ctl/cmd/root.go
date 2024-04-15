7d31e68431b3662325ef542612d83cc988ca16bc
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2024-04-15 22:05:34 +0000

    Add top level help text and version
    - Add initial Makefile that handles adding the version and other build metadata when building binaries.
    - Add initial tests via Makefile including generating a notice file and checking license compliance.

--- beegfs-ctl/cmd/beegfs-ctl/cmd/root.go
+++ beegfs-ctl/cmd/beegfs-ctl/cmd/root.go
@@ -20,12 +20,27 @@ import (
 
 // Main entry point of the tool
 func Execute() int {
-	// The root command
+	// This is the first line of the root help message. This is generated/stored here to allow the
+	// number of characters separating the header with the rest of the help text to be determined
+	// dynamically since the version width may vary.
+	longHelpHeader := fmt.Sprintf("BeeGFS Command Line Tool: %s", Version)
+	// The root command.
 	cmd := &cobra.Command{
-		Use:   "beegfs-ctl",
-		Short: "The BeeGFS command line control tool",
-		Long: `The BeeGFS command line control tool (http://www.beegfs.com)
-		`,
+		Use:   BinaryName,
+		Short: "The BeeGFS command line control tool.",
+		Long: fmt.Sprintf(`%s
+%s
+This tool allows you to inspect, configure, and monitor BeeGFS.
+
+* View help for specific commands with "<command> help".
+* For full product documentation, visit: https://doc.beegfs.io/.
+* Questions?
+  - If you have an active support contract, please visit: https://www.beegfs.io/c/enterprise/.
+  - For community support, check out the mailing list at: https://groups.google.com/g/fhgfs-user/.
+
+BeeGFS is crafted with 💛 by contributors worldwide.
+Thank you for using BeeGFS and supporting its ongoing development! 🐝
+		`, longHelpHeader, strings.Repeat("=", len(longHelpHeader))),
 		SilenceUsage: true,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if config.Get().NumWorkers < 1 {
