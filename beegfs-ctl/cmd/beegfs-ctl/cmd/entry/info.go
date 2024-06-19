aae4c3f42962c20343b49b574c3b3b206be4ac68
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2024-06-19 00:45:49 +0000

    Move reusable functions to a separate file
    Remove unnecessary duplicate case statement

--- beegfs-ctl/cmd/beegfs-ctl/cmd/entry/info.go
+++ beegfs-ctl/cmd/beegfs-ctl/cmd/entry/info.go
@@ -1,11 +1,8 @@
 package entry
 
 import (
-	"bufio"
-	"context"
 	"fmt"
 	"os"
-	"strconv"
 	"text/tabwriter"
 
 	"github.com/dsnet/golib/unitconv"
@@ -143,10 +140,6 @@ run:
 				// returning the error.
 				multiErr.Errors = append(multiErr.Errors, err)
 			}
-		case err, ok := <-errChan:
-			if ok {
-				multiErr.Errors = append(multiErr.Errors, err)
-			}
 		}
 	}
 
@@ -154,53 +147,6 @@ run:
 		return &multiErr
 	}
 	return nil
-}
-
-func getDelimiterFromString(s string) (byte, error) {
-	// The default stdin-delimiter used to print the user help won't parse correctly.
-	if s == "\n" {
-		s = `\n`
-	}
-	if unquoted, err := strconv.Unquote(`"` + s + `"`); err == nil && len(unquoted) == 1 {
-		return unquoted[0], nil
-	} else {
-		return 0, fmt.Errorf("the stdin-delimiter must be a single character or valid escape sequence such as \"\\n\" for a newline or \"\\x00\" for null (provided: %s)", s)
-	}
-}
-
-func readPathsFromStdin(ctx context.Context, delimiter byte, pathsChan chan<- string, errChan chan<- error) {
-	scanner := bufio.NewScanner(os.Stdin)
-	splitFunc := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		for i := 0; i < len(data); i++ {
-			if data[i] == delimiter {
-				return i + 1, data[:i], nil
-			}
-		}
-		// When EOF is reached, return what is left:
-		if atEOF && len(data) != 0 {
-			return len(data), data, nil
-		}
-		// Otherwise request more data
-		return 0, nil, nil
-	}
-	scanner.Split(splitFunc)
-
-	func() {
-		defer close(pathsChan)
-		for scanner.Scan() {
-			input := scanner.Text()
-			select {
-			case pathsChan <- input:
-			case <-ctx.Done():
-				errChan <- ctx.Err()
-				return
-			}
-			if err := scanner.Err(); err != nil {
-				errChan <- err
-				return
-			}
-		}
-	}()
 }
 
 func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendCfg entryInfoCfg) {
