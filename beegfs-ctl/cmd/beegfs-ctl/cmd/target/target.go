ed69a05d9b62712727b317d58c8a8a95b9e68bbd
Pranav Padmasali <19690741+pranavpadmasali@users.noreply.github.com>
2024-07-29 08:12:48 +0000

    add command set-state

--- beegfs-ctl/cmd/beegfs-ctl/cmd/target/target.go
+++ beegfs-ctl/cmd/beegfs-ctl/cmd/target/target.go
@@ -14,6 +14,7 @@ func NewCmd() *cobra.Command {
 	cmd.AddCommand(newListCmd())
 	cmd.AddCommand(newSetAliasCmd())
 	cmd.AddCommand(newDeleteCmd())
+	cmd.AddCommand(newSetStateCmd())
 
 	return cmd
 }
