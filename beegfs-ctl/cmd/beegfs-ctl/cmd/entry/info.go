c185d31ed410169c99558bbd8e162a7700443fda
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2024-08-13 21:05:31 +0000

    Update entry info mode to use new TableWrapper

--- beegfs-ctl/cmd/beegfs-ctl/cmd/entry/info.go
+++ beegfs-ctl/cmd/beegfs-ctl/cmd/entry/info.go
@@ -2,8 +2,7 @@ package entry
 
 import (
 	"fmt"
-	"os"
-	"text/tabwriter"
+	"strings"
 
 	"github.com/dsnet/golib/unitconv"
 	"github.com/spf13/cobra"
@@ -13,6 +12,7 @@ import (
 	"github.com/thinkparq/beegfs-ctl/pkg/ctl/entry"
 	"github.com/thinkparq/gobee/beegfs"
 	"github.com/thinkparq/gobee/types"
+	"go.uber.org/zap"
 )
 
 type entryInfoCfg struct {
@@ -21,10 +21,6 @@ type entryInfoCfg struct {
 	retro          bool
 	retroPaths     bool
 	verbose        bool
-	// When printing multiple rows flushing each row will result in misaligned columns. Instead
-	// buffer multiple rows and print them all at once along with their header. The flushInterval
-	// determines how "real time" the output is and how often the header is reprinted.
-	flushInterval int
 }
 
 func newEntryInfoCmd() *cobra.Command {
@@ -57,18 +53,22 @@ Alternatively multiple entries can be provided using stdin by specifying '-' as 
 	}
 
 	cmd.Flags().BoolVar(&frontendCfg.recurse, "recurse", false, "When <path> is a single directory recursively print information about all entries beneath the path (WARNING: this may return large amounts of output, for example if the BeeGFS root is the provided path).")
-	cmd.Flags().BoolVar(&frontendCfg.verbose, "verbose", false, "Print more information about each entry (such as chunk and dentry paths on the servers).")
+	// TODO: https://github.com/ThinkParQ/beegfs-ctl/issues/56
+	// Update help text when verbose no longer just applies to the retro mode.
+	cmd.Flags().BoolVar(&frontendCfg.verbose, "verbose", false, "In the \"retro\" mode, print more information about each entry, such as chunk and dentry paths on the servers.")
 	cmd.Flags().BoolVar(&frontendCfg.retro, "retro", false, "Display entry information using the \"retro\" vertical style of the original beegfs-ctl.")
 	// The same default used for stdin-delimiter to allow the help output to print correctly can't
 	// be used directly. If the default changes update where this is set in getDelimiterFromString.
 	cmd.Flags().StringVar(&frontendCfg.stdinDelimiter, "stdin-delimiter", "\n", "Change the string delimiter used to determine individual paths when read from stdin (e.g., --stdin-delimiter=\"\\x00\" for NULL).")
 	cmd.Flags().BoolVar(&frontendCfg.retroPaths, "retro-print-paths", false, "Print paths at the top of each entry in the retro output.")
 	cmd.Flags().MarkHidden("retro-print-paths")
-	cmd.Flags().IntVar(&frontendCfg.flushInterval, "flush-interval", 1000, "Set the number of lines to buffer before flushing output and reprinting the header. Increasing this number can improve alignment but decrease real-time responsiveness. Set to zero to disable printing headers.")
 	return cmd
 }
 
 func runEntryInfoCmd(cmd *cobra.Command, args []string, frontendCfg entryInfoCfg, backendCfg entry.GetEntriesConfig) error {
+
+	logger, _ := config.GetLogger()
+	log := logger.With(zap.String("component", "runEntryInfoCmd"))
 
 	// Setup the method for sending paths to the backend:
 	stdinErrChan := make(chan error, 1)
@@ -90,21 +90,26 @@ func runEntryInfoCmd(cmd *cobra.Command, args []string, frontendCfg entryInfoCfg
 	if err != nil {
 		return err
 	}
-
-	w := cmdfmt.NewDeprecatedTableWriter(os.Stdout)
-	defer w.Flush()
-	var multiErr types.MultiError
-	// When printing a table, print the header initially then only when flushing the buffer.
-	printHeader := true
-	// If the interval is zero don't ever print the header.
-	if frontendCfg.flushInterval == 0 {
-		printHeader = false
+	defaultColumns := []string{"path", "entry id", "type", "meta node", "meta mirror", "storage pool", "stripe pattern", "storage targets", "buddy groups", "remote targets", "cool down"}
+	allColumns := append(defaultColumns, "client sessions")
+	numColumns := len(allColumns)
+	var tbl cmdfmt.TableWrapper
+	if frontendCfg.retro {
+		// For simplicity still use the table wrapper to handle printing the retro output. Just put
+		// all the output into one unlabeled column.
+		tbl = cmdfmt.NewTableWrapper([]string{}, []string{})
+	} else {
+		if frontendCfg.verbose {
+			defaultColumns = allColumns
+		}
+		tbl = cmdfmt.NewTableWrapper(allColumns, defaultColumns)
 	}
-	count := 0
+	defer tbl.PrintRemaining()
+
+	var multiErr types.MultiError
 
 run:
 	for {
-		count++
 		select {
 		case info, ok := <-entriesChan:
 			if !ok {
@@ -112,19 +117,17 @@ run:
 			}
 			// Still try and print as much information as possible if verbose details are not available.
 			if len(info.Entry.Verbose.Err.Errors) != 0 {
-				fmt.Fprintf(&w, "warning: unable to generate all output needed to print verbose details, some information may be missing: %s (ignoring)\n", info.Entry.Verbose.Err.Error())
+				log.Warn("unable to generate all output needed to print verbose details, some information may be missing (ignoring)", zap.Error(err))
 			}
 			if frontendCfg.retro {
-				printRetro(&w, info, frontendCfg)
+				tbl.Row(assembleRetroEntry(info, frontendCfg))
 			} else {
-				printTable(&w, info, printHeader, frontendCfg)
-				printHeader = false
-			}
-			if frontendCfg.flushInterval > 0 && count%frontendCfg.flushInterval == 0 {
-				printHeader = true
-				w.Flush()
-			} else if frontendCfg.flushInterval == 0 {
-				w.Flush()
+				row := assembleTableRow(info, numColumns)
+				if len(row) != numColumns {
+					// Sanity check in case new columns are added and assembleTableRow() is not updated.
+					log.Warn("number of columns in the row does not equal the expected number of columns, this is likely a bug and table output will probably not be formatted correctly (ignoring)", zap.Any("expected", numColumns), zap.Any("actual", len(row)))
+				}
+				tbl.Row(row...)
 			}
 		case err, ok := <-errChan:
 			if ok {
@@ -142,15 +145,16 @@ run:
 	return nil
 }
 
-func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendCfg entryInfoCfg) {
+func assembleRetroEntry(info *entry.GetEntryCombinedInfo, frontendCfg entryInfoCfg) string {
+	entryToPrint := &strings.Builder{}
 
 	if frontendCfg.retroPaths {
-		fmt.Fprintf(w, "Path: %s\n", info.Path)
+		fmt.Fprintf(entryToPrint, "Path: %s\n", info.Path)
 	}
-	fmt.Fprintf(w, "Entry type: %s\n", info.Entry.Type)
-	fmt.Fprintf(w, "EntryID: %s\n", info.Entry.EntryID)
+	fmt.Fprintf(entryToPrint, "Entry type: %s\n", info.Entry.Type)
+	fmt.Fprintf(entryToPrint, "EntryID: %s\n", info.Entry.EntryID)
 	if info.Entry.Type == beegfs.EntryDirectory && frontendCfg.verbose {
-		fmt.Fprintf(w, "ParentID: %s\n", info.Entry.ParentEntryID)
+		fmt.Fprintf(entryToPrint, "ParentID: %s\n", info.Entry.ParentEntryID)
 	}
 
 	// When printing verbose details meta node info is printed later in the output and printed
@@ -162,10 +166,10 @@ func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendC
 			frontText = " + "
 		}
 		if entry.FeatureFlags.IsBuddyMirrored() {
-			fmt.Fprintf(w, "%sMetadata buddy group: %d\n", frontText, entry.MetaBuddyGroup)
-			fmt.Fprintf(w, "%sCurrent primary metadata node: %s [ID: %d]\n", frontText, entry.MetaOwnerNode.Alias, entry.MetaOwnerNode.Id.NumId)
+			fmt.Fprintf(entryToPrint, "%sMetadata buddy group: %d\n", frontText, entry.MetaBuddyGroup)
+			fmt.Fprintf(entryToPrint, "%sCurrent primary metadata node: %s [ID: %d]\n", frontText, entry.MetaOwnerNode.Alias, entry.MetaOwnerNode.Id.NumId)
 		} else {
-			fmt.Fprintf(w, "%sMetadata node: %s [ID: %d]\n", frontText, entry.MetaOwnerNode.Alias, entry.MetaOwnerNode.Id.NumId)
+			fmt.Fprintf(entryToPrint, "%sMetadata node: %s [ID: %d]\n", frontText, entry.MetaOwnerNode.Alias, entry.MetaOwnerNode.Id.NumId)
 		}
 	}
 
@@ -173,34 +177,34 @@ func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendC
 		printMetaNodeInfo(info.Entry, false)
 	}
 
-	fmt.Fprintf(w, "Stripe pattern details:\n")
-	fmt.Fprintf(w, "+ Type: %s\n", info.Entry.Pattern.Type)
+	fmt.Fprintf(entryToPrint, "Stripe pattern details:\n")
+	fmt.Fprintf(entryToPrint, "+ Type: %s\n", info.Entry.Pattern.Type)
 	if viper.GetBool(config.RawKey) {
-		fmt.Fprintf(w, "+ Chunksize: %d\n", info.Entry.Pattern.Chunksize)
+		fmt.Fprintf(entryToPrint, "+ Chunksize: %d\n", info.Entry.Pattern.Chunksize)
 	} else {
-		fmt.Fprintf(w, "+ Chunksize: %s\n", unitconv.FormatPrefix(float64(info.Entry.Pattern.Chunksize), unitconv.Base1024, 0))
+		fmt.Fprintf(entryToPrint, "+ Chunksize: %s\n", unitconv.FormatPrefix(float64(info.Entry.Pattern.Chunksize), unitconv.Base1024, 0))
 	}
-	fmt.Fprintf(w, "+ Number of storage targets: ")
+	fmt.Fprintf(entryToPrint, "+ Number of storage targets: ")
 	actualNumStorageTgts := len(info.Entry.Pattern.TargetIDs)
 	if actualNumStorageTgts == 0 {
 		// Expected if this is a directory.
-		fmt.Fprintf(w, "desired: %d\n", info.Entry.Pattern.DefaultNumTargets)
+		fmt.Fprintf(entryToPrint, "desired: %d\n", info.Entry.Pattern.DefaultNumTargets)
 	} else {
-		fmt.Fprintf(w, "desired: %d; actual: %d\n", info.Entry.Pattern.DefaultNumTargets, actualNumStorageTgts)
+		fmt.Fprintf(entryToPrint, "desired: %d; actual: %d\n", info.Entry.Pattern.DefaultNumTargets, actualNumStorageTgts)
 	}
 
 	if info.Entry.Type == beegfs.EntryDirectory {
-		fmt.Fprintf(w, "+ Storage Pool: %d  (%s)\n", info.Entry.Pattern.StoragePoolID, info.Entry.Pattern.StoragePoolName)
+		fmt.Fprintf(entryToPrint, "+ Storage Pool: %d  (%s)\n", info.Entry.Pattern.StoragePoolID, info.Entry.Pattern.StoragePoolName)
 	}
 
 	if actualNumStorageTgts != 0 {
 		if info.Entry.Pattern.Type == beegfs.StripePatternBuddyMirror {
-			fmt.Fprintf(w, "+ Storage mirror buddy groups:\n")
+			fmt.Fprintf(entryToPrint, "+ Storage mirror buddy groups:\n")
 			for _, tgt := range info.Entry.Pattern.TargetIDs {
-				fmt.Fprintf(w, "  + %d\n", tgt)
+				fmt.Fprintf(entryToPrint, "  + %d\n", tgt)
 			}
 		} else {
-			fmt.Fprintf(w, "+ Storage targets:\n")
+			fmt.Fprintf(entryToPrint, "+ Storage targets:\n")
 			for tgt, node := range info.Entry.Pattern.StorageTargets {
 				// Here we differ slightly from the old CTL. The old CTL method of getting target mappings
 				// first determined the node ID, then used this to determine the alias. It was possible
@@ -208,9 +212,9 @@ func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendC
 				// The new target mapper works differently, looking up both the node ID and alias at once,
 				// so if anything goes wrong we can't even print the node ID.
 				if node == nil {
-					fmt.Fprintf(w, "+ %d @ <unknown>\n", tgt)
+					fmt.Fprintf(entryToPrint, "+ %d @ <unknown>\n", tgt)
 				} else {
-					fmt.Fprintf(w, "+ %d @ %s [ID: %d]\n", tgt, node.Alias, node.LegacyId.NumId)
+					fmt.Fprintf(entryToPrint, "+ %d @ %s [ID: %d]\n", tgt, node.Alias, node.LegacyId.NumId)
 				}
 
 			}
@@ -218,125 +222,117 @@ func printRetro(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, frontendC
 	}
 
 	if len(info.Entry.Remote.Targets) != 0 {
-		fmt.Fprintf(w, "Remote Storage Target Details:\n")
-		fmt.Fprintf(w, "+ CoolDown: %d\n", info.Entry.Remote.CoolDownPeriod)
-		fmt.Fprintf(w, "+ Remote Storage Targets:\n")
+		fmt.Fprintf(entryToPrint, "Remote Storage Target Details:\n")
+		fmt.Fprintf(entryToPrint, "+ CoolDown: %d\n", info.Entry.Remote.CoolDownPeriod)
+		fmt.Fprintf(entryToPrint, "+ Remote Storage Targets:\n")
 		for rstID, rst := range info.Entry.Remote.Targets {
 			if rst == nil {
-				fmt.Fprintf(w, "  + <not available> [ID: %d]\n", rstID)
+				fmt.Fprintf(entryToPrint, "  + <not available> [ID: %d]\n", rstID)
 			} else {
-				fmt.Fprintf(w, "  + %s [ID: %d]\n", rst.Name, rstID)
+				fmt.Fprintf(entryToPrint, "  + %s [ID: %d]\n", rst.Name, rstID)
 			}
 		}
 	}
 
 	if frontendCfg.verbose {
 		if info.Entry.Type.IsFile() {
-			fmt.Fprintf(w, "Chunk path: %s\n", info.Entry.Verbose.ChunkPath)
+			fmt.Fprintf(entryToPrint, "Chunk path: %s\n", info.Entry.Verbose.ChunkPath)
 		}
 		switch info.Entry.FeatureFlags.IsInlined() {
 		case true:
-			fmt.Fprintf(w, "Inlined inode: yes\n")
+			fmt.Fprintf(entryToPrint, "Inlined inode: yes\n")
 		default:
-			fmt.Fprintf(w, "Inlined inode: no\n")
+			fmt.Fprintf(entryToPrint, "Inlined inode: no\n")
 		}
 		if info.Entry.EntryID != "root" && len(info.Entry.Verbose.DentryPath) != 0 {
-			fmt.Fprintf(w, "Dentry info:\n")
-			fmt.Fprintf(w, " + Path: %s\n", info.Entry.Verbose.DentryPath[1:])
+			fmt.Fprintf(entryToPrint, "Dentry info:\n")
+			fmt.Fprintf(entryToPrint, " + Path: %s\n", info.Entry.Verbose.DentryPath[1:])
 			printMetaNodeInfo(info.Parent, true)
 		}
 		if !info.Entry.FeatureFlags.IsInlined() && len(info.Entry.Verbose.HashPath) != 0 {
-			fmt.Fprintf(w, "Inode info:\n")
-			fmt.Fprintf(w, " + Path: %s\n", info.Entry.Verbose.HashPath[1:])
+			fmt.Fprintf(entryToPrint, "Inode info:\n")
+			fmt.Fprintf(entryToPrint, " + Path: %s\n", info.Entry.Verbose.HashPath[1:])
 			printMetaNodeInfo(info.Entry, true)
 		}
 		if info.Entry.Type == beegfs.EntryRegularFile {
-			fmt.Fprintf(w, "Client Sessions\n")
-			fmt.Fprintf(w, "+ Reading: %d\n+ Writing: %d\n", info.Entry.NumSessionsRead, info.Entry.NumSessionsWrite)
+			fmt.Fprintf(entryToPrint, "Client Sessions\n")
+			fmt.Fprintf(entryToPrint, "+ Reading: %d\n+ Writing: %d\n", info.Entry.NumSessionsRead, info.Entry.NumSessionsWrite)
 		}
 	}
-	fmt.Fprintf(w, "\n")
+	return entryToPrint.String()
 }
 
 // TODO: https://github.com/ThinkParQ/beegfs-ctl/issues/56
 // Finish implementing (mainly just verbose output).
-func printTable(w *tabwriter.Writer, info *entry.GetEntryCombinedInfo, printHeader bool, frontendCfg entryInfoCfg) {
-	if printHeader {
-		fmt.Fprintf(w, "Path\tEntry ID\tType\tMeta Node\tMeta Mirror\tStorage Pool\tStripe Pattern\tStorage Targets\tBuddy Groups\tRemote Targets\tCool Down")
-		if frontendCfg.verbose {
-			fmt.Fprintf(w, "\tClient Sessions")
-		}
-		fmt.Fprintf(w, "\n")
-	}
-	entryRow := fmt.Sprintf("%s\t", info.Path)
-	entryRow += fmt.Sprintf("%s\t%s\t", info.Entry.EntryID, info.Entry.Type)
-	entryRow += fmt.Sprintf("%s (%d)\t", info.Entry.MetaOwnerNode.Alias, info.Entry.MetaOwnerNode.Id.NumId)
+func assembleTableRow(info *entry.GetEntryCombinedInfo, rowLen int) []any {
+	row := make([]any, 0, rowLen)
+
+	row = append(row,
+		info.Path,
+		info.Entry.EntryID,
+		info.Entry.Type,
+		fmt.Sprintf("%s (%d)", info.Entry.MetaOwnerNode.Alias, info.Entry.MetaOwnerNode.Id.NumId),
+	)
 
 	if info.Entry.FeatureFlags.IsBuddyMirrored() {
-		entryRow += fmt.Sprintf("%d\t", info.Entry.MetaBuddyGroup)
+		row = append(row, info.Entry.MetaBuddyGroup)
 	} else {
-		entryRow += "(unmirrored)\t"
+		row = append(row, "(unmirrored)")
 	}
 
-	entryRow += fmt.Sprintf("%s (%d)\t", info.Entry.Pattern.StoragePoolName, info.Entry.Pattern.StoragePoolID)
+	row = append(row, fmt.Sprintf("%s (%d)", info.Entry.Pattern.StoragePoolName, info.Entry.Pattern.StoragePoolID))
 
 	if viper.GetBool(config.RawKey) {
-		entryRow += fmt.Sprintf("%s (%dx%d)\t", info.Entry.Pattern.Type, info.Entry.Pattern.DefaultNumTargets, info.Entry.Pattern.Chunksize)
+		row = append(row, fmt.Sprintf("%s (%dx%d)", info.Entry.Pattern.Type, info.Entry.Pattern.DefaultNumTargets, info.Entry.Pattern.Chunksize))
 	} else {
-		entryRow += fmt.Sprintf("%s (%dx%s)\t", info.Entry.Pattern.Type, info.Entry.Pattern.DefaultNumTargets, unitconv.FormatPrefix(float64(info.Entry.Pattern.Chunksize), unitconv.Base1024, 0))
+		row = append(row, fmt.Sprintf("%s (%dx%s)", info.Entry.Pattern.Type, info.Entry.Pattern.DefaultNumTargets, unitconv.FormatPrefix(float64(info.Entry.Pattern.Chunksize), unitconv.Base1024, 0)))
 	}
 
-	if info.Entry.Type != beegfs.EntryDirectory {
-		if info.Entry.Pattern.Type == beegfs.StripePatternBuddyMirror {
-			entryRow += "(mirrored)\t"
-			numCommas := len(info.Entry.Pattern.TargetIDs) - 1
-			for i, tgt := range info.Entry.Pattern.TargetIDs {
-				entryRow += fmt.Sprintf("%d", tgt)
-				if i < numCommas {
-					entryRow += ","
-				}
+	fmtTgtIDsFunc := func(targetIDs []uint16) string {
+		var targetsBuilder strings.Builder
+		for i, tgt := range targetIDs {
+			if i > 0 {
+				targetsBuilder.WriteString(",")
 			}
-			entryRow += "\t"
-		} else {
-			numCommas := len(info.Entry.Pattern.TargetIDs) - 1
-			for i, tgt := range info.Entry.Pattern.TargetIDs {
-				entryRow += fmt.Sprintf("%d", tgt)
-				if i < numCommas {
-					entryRow += ","
-				}
-			}
-			entryRow += "\t(unmirrored)\t"
+			targetsBuilder.WriteString(fmt.Sprintf("%d", tgt))
 		}
+		return targetsBuilder.String()
+	}
+
+	if info.Entry.Type == beegfs.EntryDirectory {
+		row = append(row, "(directory)", "(directory)")
 	} else {
-		entryRow += "(directory)\t(directory)\t"
+		if info.Entry.Pattern.Type == beegfs.StripePatternBuddyMirror {
+			row = append(row, "(mirrored)", fmtTgtIDsFunc(info.Entry.Pattern.TargetIDs))
+		} else {
+			row = append(row, fmtTgtIDsFunc(info.Entry.Pattern.TargetIDs), "(unmirrored)")
+		}
 	}
 
 	if len(info.Entry.Remote.Targets) != 0 {
 		i := 0
-		numCommas := len(info.Entry.Remote.Targets) - 1
+		var rstBuilder strings.Builder
 		for rstID, rst := range info.Entry.Remote.Targets {
-			if rst == nil {
-				entryRow += fmt.Sprintf("%d-not-available", rstID)
-			} else {
-				entryRow += fmt.Sprintf("%d-%s", rstID, rst.Name)
+			if i > 0 {
+				rstBuilder.WriteString(",")
 			}
-			if i < numCommas {
-				entryRow += ","
+			if rst == nil {
+				rstBuilder.WriteString(fmt.Sprintf("%d-not-available", rstID))
+			} else {
+				rstBuilder.WriteString(fmt.Sprintf("%d-%s", rstID, rst.Name))
 			}
 			i++
 		}
-		entryRow += fmt.Sprintf("\t%ds", info.Entry.Remote.CoolDownPeriod)
+		row = append(row, rstBuilder.String(), fmt.Sprintf("%ds", info.Entry.Remote.CoolDownPeriod))
 	} else {
-		entryRow += "(none)\t(n/a)"
+		row = append(row, "(none)", "(n/a)")
 	}
 
-	if frontendCfg.verbose {
-		if info.Entry.Type == beegfs.EntryRegularFile {
-			entryRow += fmt.Sprintf("\tReading: %d, Writing: %d", info.Entry.NumSessionsRead, info.Entry.NumSessionsWrite)
-		} else {
-			entryRow += "\tN/A"
-		}
+	if info.Entry.Type == beegfs.EntryRegularFile {
+		row = append(row, fmt.Sprintf("Reading: %d, Writing: %d", info.Entry.NumSessionsRead, info.Entry.NumSessionsWrite))
+	} else {
+		row = append(row, "(directory)")
 	}
 
-	fmt.Fprintf(w, "%s\n", entryRow)
+	return row
 }
