bb749434ae0a2b7c0c5dd620cf379857a58b7462
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2024-10-28 14:20:36 +0000

    Parse whole numbers from strings without precision loss
    Parsing uint64 bytes from a string now only uses floats if the string contains a decimal. This avoids precision loss with large whole numbers.
    
    Errors are now consistently returned if the result would overflow a uint64.

--- ctl/internal/util/parser_test.go
+++ ctl/internal/util/parser_test.go
@@ -23,21 +23,30 @@ func TestParseIntFromStr(t *testing.T) {
 		{"1.5Mi", 1572864, false}, // 1.5 * 1024 * 1024
 		{"1G", 1000000000, false},
 		{"1GB", 1000000000, false},
-		{"2.5T", 2500000000000, false}, // 2.5 * 1000 * 1000 * 1000 * 1000
-		{"3.2Gi", 3435973836, false},   // 3.2 * 1024 * 1024 * 1024
-		{"1536ki", 1572864, false},     // equivalent to 1.5Mi in bytes
-		{"", 0, true},                  // invalid input
-		{"10XY", 0, true},              // invalid prefix
-		{"1.5.5G", 0, true},            // invalid number format
+		{"2.5T", 2500000000000, false},                        // 2.5 * 1000 * 1000 * 1000 * 1000
+		{"3.2Gi", 3435973836, false},                          // 3.2 * 1024 * 1024 * 1024
+		{"1536ki", 1572864, false},                            // equivalent to 1.5Mi in bytes
+		{"", 0, true},                                         // invalid input
+		{"10XY", 0, true},                                     // invalid prefix
+		{"1.5.5G", 0, true},                                   // invalid number format
+		{"18446744073709551615", 18446744073709551615, false}, // Max uint64 in bytes does not return an error.
+		{"18446744073709551616", 0, true},                     // Max uint64+1 in bytes returns an error.
+		{"9007199254740992.0", 0, true},                       // Bytes must be specified as a whole number.
+		{"8.00PiB", 9007199254740992, false},                  // There is no loss of precision up to 9007199254740992 for decimal values.
+		{"9PiB", 10133099161583616, false},                    // There is no loss of precision above 9007199254740992 for whole numbers.
+		{"18014398509481984KiB", 0, true},                     // Converting whole numbers that would overflow a uint64 results in an error.
+		{"18014398509481984.00KiB", 0, true},                  // Converting decimals that would overflow a uint64 results in an error.
+		{"1kiI", 0, true},                                     // Reject if the unit is not B for bytes.
+		{"1kib", 1024, true},                                  // Reject if the unit is not an uppercase B for bytes.
 	}
 
 	for _, test := range tests {
 		result, err := ParseIntFromStr(test.input)
 		if test.err {
-			assert.Error(t, err, "expected an error for input %q", test.input)
+			assert.Error(t, err, "expected an error for input %s", test.input)
 		} else {
-			assert.NoError(t, err, "did not expect an error for input %q", test.input)
-			assert.Equal(t, test.expected, result, "unexpected result for input %q", test.input)
+			assert.NoError(t, err, "did not expect an error for input %s", test.input)
+			assert.Equal(t, test.expected, result, "unexpected result for input %s", test.input)
 		}
 	}
 }
