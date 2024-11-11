8fd6b13d45f522b17d96aaa9d04e837fe066fb97
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2024-11-11 07:43:40 -0600

    Update path to copy binary, add threads flag, update docs (#67)
    * Update path to copy binary, add threads flag, update docs
    
    The num-workers flag is used to automatically determine the number of Goroutines CTL should use.
    This might be confusing for the copy mode where threads often applies to other nodes, not just this one.
    So it is better to have a dedicated flag for threads.
    This also makes it more straightforward to customize the default and hold one thread back for CTL.
    
    Incorporates the remaining man page docs into the copy help.

--- ctl/internal/cmd/copy/copy.go
+++ ctl/internal/cmd/copy/copy.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/spf13/cobra"
 	"github.com/thinkparq/beegfs-go/ctl/internal/bflag"
@@ -16,7 +17,7 @@ import (
 )
 
 const (
-	beegfsCopyPath = "/opt/beegfs/sbin/beegfs-copy/bin/beegfs-copy"
+	beegfsCopyPath = "/opt/beegfs/sbin/beegfs-copy"
 	featureString  = "io.beegfs.copy"
 )
 
@@ -29,43 +30,96 @@ func NewCopyCmd() *cobra.Command {
 	var bflagSet *bflag.FlagSet
 	var frontendCfg frontendCfg
 	cmd := &cobra.Command{
-		Use:     "copy <source> [<source>] <destination>",
+		Use:     "copy -m <machine-file> <source> [<source>] <destination>",
 		Args:    cobra.MinimumNArgs(2),
 		Aliases: []string{"cp"},
 		Short:   "Copy files and directories in parallel.",
-		Long: fmt.Sprintf(`Copy files and directories in parallel.
+		Long: `Copy files and directories in parallel.
 
-Specifying Paths:
-One or more <source> paths can be copied under a <destination> directory.
+SPECIFYING PATHS:
+One or more <source> paths can be copied under a <destination> directory. The <destination> must be a directory (not a file).
 When supported by the current shell, standard wildcards (globbing patterns) can be used in each path.
 Alternatively paths can be provided using stdin by using '-' as the <source> (example: 'cat file_list.txt | beegfs copy - <destination>').
+When copying using multiple nodes, ensure the source path(s) and destination are mounted to the same location on all nodes.
 
-Parallelism:
+PARALLELISM:
 This mode supports two levels of parallelism:
 
-* Thread parallelism using multiple threads per node based on the --%s flag (by default the number of cores on this machine).
+* Thread parallelism using multiple threads per node based on the --threads flag (by default the number of cores on this machine).
 * Node level parallelism using one or more machines specified in a machine file.
 
 Passwordless SSH must be setup to all the nodes in the machine file, including the localhost if specified.
 A machine file is always required and the simplest file could contain a single entry for the local machine:
 
-machinefile.txt
-  localhost
+  machinefile.txt
+    localhost
 
 If the same BeeGFS instance or network share is mounted to multiple nodes, multiple machines can be used to copy data in parallel.
 A path to a machine file containing a list of nodes to use to execute the copy operation must be provided:
 
-machinefile.txt
-  node01
-  node02
-  node03
+  machinefile.txt
+    node01
+    node02
+    node03
+
+EXAMPLES:
+Copy file1 and file2 to /path2/file1 and /path2/file2 using a single thread per node (-t 1):
+
+  $ beegfs copy -m /tmp/machinefile -t 1 /path1/file1 /path1/file1 /path2
+
+Copy folder /many_files/ and its contents to /path2/many_files/ using 8 threads per node (-t 8):
+
+  $ beegfs copy -m /tmp/machinefile -t 8 /path1/many_files/ /path2
+
+Copy two files, one 100MB and one 200GB with a partition threshold of 5GB (-p 5120):
+
+  $ beegfs copy -m /tmp/machinefile -t 8 -p 5120 /path1/file100MB.file /path1/file200GB.file /path2
+
+Copy one file specifying the chunksize for each thread as 512MB (-c 512):
+
+  $ beegfs copy m /tmp/machinefile -t 8 -c 512 /path1/bigData.file /path2
+
+Copy a file with all possible options enabled: 
+
+  $ beegfs copy -m /tmp/machinefile -n 4 -t 8 -p 1024 -c 512 -v 3 -s /path1/file_256GB.file /path2
+
+This would copy using first four nodes in the machinefile with 8 threads per node, a partition threshold
+of 1024MB and chunksize of 512MB while printing out statistics and detailed logs about the copy process.
+The nodes, threads, and partition configuration would result in 32 threads each copying 8GB of data.
+
+Compare the contents of one folder with another:
+
+  $ beegfs copy -m /tmp/machinefile -t 8 /path1/many_files_and_dir /path2/many_files_and_dir -l
+
+This would list the missing subdirectories and files in the destination path, along with any files
+with different sizes or older modification timestamps in the destination. No changes will be made.
+This mode only accepts once source path which can be a file or directory, but the destination type
+must match the source type.
+
+Update a destination path and its contents based on the source path:
+
+  $ beegfs-copy -m /tmp/machinefile -t 8 /path1/many_files_and_dir /path2/many_files_and_dir -u
+
+This will compare the source and destination path, only copying contents that are missing based on the
+same rules defined above for comparing source and destination paths.
+
+LIMITATIONS:
+* Modifying the source/destination directory tree structure while copy is running is discouraged, and
+  alterations could lead to incomplete copies of the directory. The update mode when running on a 
+  single node does not have this limitations; only multi-node updates that must first prepare a list of
+  changed files before distributing them across multiple nodes is affected. The subsequent copy process
+  is agnostic to modifications of the directory structure.
+* When copying large directory structures, the copy mode could reach the open files limit (ulimit -n)
+  and exit return a message like: "Failed to open directory". If necessary, increase this limit.
+* Avoid copying multiple source paths with the same base name, since the final copy may be different
+  from what is expected. For example avoid a copy like:
+  * $ beegfs copy -m machinefile dir1/dir dir2/dir dir3/dir destination
 
 IMPORTANT: The primary use case of the copy mode is staging data for compute jobs.
 As such it does not validate after the copy completes that the source was not modified in the meantime.
 For performance it also does not perform any checksum verification when comparing the source/destination.
 If used as part of a backup or other workload where strict consistency guarantees are required, users
-should take measures to perform additional verification the source and destination match bit-for-bit.
-`, config.NumWorkersKey),
+should take measures to perform additional verification the source and destination match bit-for-bit.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			_, err := os.Stat(beegfsCopyPath)
 			if err != nil {
@@ -91,14 +145,25 @@ should take measures to perform additional verification the source and destinati
 			return copyRunner(bflagSet, args[:len(args)-1], args[len(args)-1])
 		},
 	}
+
+	defaultThreads := runtime.GOMAXPROCS(0)
+	if defaultThreads > 1 {
+		// If there is more than one CPU, reserve one for CTL itself.
+		defaultThreads = defaultThreads - 1
+	}
+
 	copyFlags := []bflag.FlagWrapper{
 		bflag.Flag("machine-file", "m", "Path to a file containing a list of nodes that should be used to execute this copy.", "-m", ""),
-		bflag.GlobalFlag(config.NumWorkersKey, "-t"),
-		bflag.Flag("nodes", "n", "Start this many from machine file. If set to zero all nodes will be used.", "-n", 0),
-		bflag.Flag("keep-atime", "a", "Do not modify the access time of the source file(s).", "-a", false),
-		bflag.Flag("chunksize", "c", "Chunk size for copy operations (in MB).", "-c", ""),
-		bflag.Flag("keep-mtime", "k", "Keep the original modification time from the source in the destination.", "-k", false),
-		bflag.Flag("partition-threshold", "p", "Partition copy threshold (in MB).", "-p", ""),
+		bflag.Flag("threads", "t", "Number of threads per node. The default is the number of CPUs on this machine minus one, or one if there is a single CPU.", "-t", defaultThreads),
+		bflag.Flag("nodes", "n", "Start the copy using this many nodes from the machine file (selected top to bottom). The default (0) will use all nodes.", "-n", 0),
+		bflag.Flag("keep-atime", "a", `Do not modify the access time of the source file(s). 
+This only works if supported and enabled on the source file system.`, "-a", false),
+		bflag.Flag("chunksize", "c", "Chunk size for copy operations (in MB).", "-c", 128),
+		bflag.Flag("keep-mtime", "k", `Keep the original modification time from the source in the destination. 
+This only works if supported and enabled on the destination file system.`, "-k", false),
+		bflag.Flag("partition-threshold", "p", `Partition copy threshold (in MB).
+To accelerate the copy operation, large files are partitioned and copied using multiple threads.
+Files with a size below the partition-threshold are processed by a single thread.`, "-p", 1000),
 		bflag.Flag("list-diff", "l", `Do not make any changes and just list differences between the source and destination paths. 
 This mode will list missing files and directories, and files with different sizes or older
 modification times in the destination. Only one source path is accepted, and can be either 
@@ -116,12 +181,15 @@ destination have different modification times.`,
 			"-d", false),
 		bflag.Flag("statistics", "s", "Print thread statistics.", "-s", false),
 		// Note I have yet to find a good way to detect if the debug flag is set and automatically set "-v 3".
-		bflag.Flag("verbose", "v", "Increase output verbosity. Levels 1-3 are supported.", "-v", ""),
+		bflag.Flag("verbose", "v", `Increase output verbosity. Levels 1-3 are supported.
+1 - Print general information about the copy process such as the specified path, start, and finish. 
+2 - Also print the files and folders that were copied.
+3 - Debug logging including details about the reading and writing process.`, "-v", ""),
 	}
 
 	bflagSet = bflag.NewFlagSet(copyFlags, cmd)
 	cmd.Flags().StringVar(&frontendCfg.stdinDelimiter, "stdin-delimiter", "\\n", `Change the string delimiter used to determine individual paths when read from stdin.
-		For example use --stdin-delimiter=\"\\x00\" for NULL.`)
+For example use --stdin-delimiter=\"\\x00\" for NULL.`)
 	cmd.Flags().IntVar(&frontendCfg.batchSize, "stdin-batch", 1024, "At most this many paths will be read from stdin before triggering the parallel copy. Setting to higher values will consume more memory.")
 	cmd.MarkFlagRequired("machine-file")
 	cmd.Flags().MarkHidden("copy-help")
