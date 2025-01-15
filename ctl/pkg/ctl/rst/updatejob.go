28021f3e93069eaa13ac233ea34d59b2ac0d4a6f
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2025-01-15 00:29:53 +0000

    Remote job commands allow recursively updating jobs and limiting updates by remote target
    * Frontend job cancel and delete commands now use a common updateJobRunner.
    * Backend UpdateJobs function has been reworked into an UpdateJobsByPaths function that calls either the original UpdateJobs RPC or new streaming UpdatePaths RPC and returns results over a channel.

--- ctl/pkg/ctl/rst/updatejob.go
+++ ctl/pkg/ctl/rst/updatejob.go
@@ -3,60 +3,126 @@ package rst
 import (
 	"context"
 	"errors"
+	"fmt"
+	"io"
+	"math"
 
 	"github.com/thinkparq/beegfs-go/common/filesystem"
 	"github.com/thinkparq/beegfs-go/ctl/pkg/config"
 	"github.com/thinkparq/protobuf/go/beeremote"
+	"google.golang.org/protobuf/proto"
 )
 
 type UpdateJobCfg struct {
-	Path     string
-	JobID    string
-	Force    bool
-	NewState beeremote.UpdateJobRequest_NewState
+	// The path or path prefix to update (when Recurse is set).
+	Path          string
+	JobID         string
+	RemoteTargets []uint
+	Force         bool
+	NewState      beeremote.UpdateJobsRequest_NewState
+	Recurse       bool
 }
 
-// UpdateJobs updates the state of all jobs at the specified path. This function intentionally does
-// not check if the provided path still exists in BeeGFS because a job could be triggered then the
-// file deleted/renamed. For similar reasons it does not check if the path is a directory (which
-// shouldn't normally have jobs), since it is possible a file was uploaded, deleted, then a
-// directory created in its place with the same name. In all of these cases we need to be able to
-// update any existing jobs for any path. If no jobs exist, Remote will simply return not found.
-//
-// TODO (https://github.com/ThinkParQ/bee-remote/issues/15): Support updating multiple paths based
-// on a provided prefix.
-func UpdateJobs(ctx context.Context, cfg UpdateJobCfg) (*beeremote.UpdateJobResponse, error) {
+type UpdateJobsResponse struct {
+	Path   string
+	Result *beeremote.UpdateJobsResponse
+	Err    error
+}
+
+// UpdateJobsByPaths updates the state of one or more job(s) for one or more path(s). This function
+// intentionally does not check if the provided path still exists in BeeGFS because a job could be
+// triggered then the file deleted/renamed. For similar reasons it does not check if the path is a
+// directory (which shouldn't normally have jobs), since it is possible a file was uploaded,
+// deleted, then a directory created in its place with the same name. In all of these cases we need
+// to be able to update any existing jobs for any path. If no jobs exist, Remote will simply return
+// not found.
+func UpdateJobsByPaths(ctx context.Context, cfg *UpdateJobCfg, respChan chan<- *UpdateJobsResponse) error {
+
+	if cfg.Recurse && cfg.JobID != "" {
+		return errors.New("invalid configuration: cannot both recursively update jobs and update a specific job ID")
+	}
 
 	beegfs, err := config.BeeGFSClient(cfg.Path)
 	if err != nil && !errors.Is(err, filesystem.ErrUnmounted) {
-		return nil, err
+		return err
 	}
 	pathInMount, err := beegfs.GetRelativePathWithinMount(cfg.Path)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	request := &beeremote.UpdateJobRequest{
+	request := beeremote.UpdateJobsRequest_builder{
 		NewState:    cfg.NewState,
 		ForceUpdate: cfg.Force,
-	}
+		Path:        pathInMount,
+	}.Build()
 
-	switch {
-	case cfg.JobID != "":
-		request.Query = &beeremote.UpdateJobRequest_ByIdAndPath{
-			ByIdAndPath: &beeremote.UpdateJobRequest_QueryIdAndPath{
-				JobId: cfg.JobID,
-				Path:  pathInMount,
-			},
+	if cfg.JobID != "" {
+		request.SetJobId(*proto.String(cfg.JobID))
+	}
+	if cfg.RemoteTargets != nil {
+		remoteTargets := map[uint32]bool{}
+		for _, tgt := range cfg.RemoteTargets {
+			if tgt > math.MaxUint32 {
+				return fmt.Errorf("specified remote target ID is larger than the uint32 maximum: %d", tgt)
+			}
+			remoteTargets[uint32(tgt)] = true
 		}
-	case pathInMount != "":
-		request.Query = &beeremote.UpdateJobRequest_ByExactPath{ByExactPath: pathInMount}
+		request.SetRemoteTargets(remoteTargets)
 	}
 
 	beeRemote, err := config.BeeRemoteClient()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return beeRemote.UpdateJob(ctx, request)
+	// For a single path just use the unary RPC to update jobs. Runs in a separate goroutine so the
+	// caller can wait for UpdateJobs() to return before reading from the channel.
+	if !cfg.Recurse {
+		go func() {
+			defer close(respChan)
+			resp, err := beeRemote.UpdateJobs(ctx, request)
+			if err != nil {
+				respChan <- &UpdateJobsResponse{
+					Err: err,
+				}
+				return
+			}
+			respChan <- &UpdateJobsResponse{
+				Path:   request.Path,
+				Result: resp,
+			}
+		}()
+		return nil
+	}
+
+	// Otherwise use the streaming RPC to recursively update all paths with this prefix.
+	stream, err := beeRemote.UpdatePaths(ctx, beeremote.UpdatePathsRequest_builder{
+		PathPrefix:      pathInMount,
+		RequestedUpdate: request,
+	}.Build())
+
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer close(respChan)
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				respChan <- &UpdateJobsResponse{
+					Err: err,
+				}
+				return
+			}
+			respChan <- &UpdateJobsResponse{
+				Path:   resp.GetPath(),
+				Result: resp.GetUpdateResult(),
+			}
+		}
+	}()
+	return nil
 }
--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
-	github.com/thinkparq/protobuf v0.0.0-beta.4.0.20250116091432-31cdc1397691
+	github.com/thinkparq/protobuf v0.0.0-beta.4.0.20250124154812-3dd9c71631dd
 	go.uber.org/zap v1.27.0
 	golang.org/x/sys v0.29.0
 	golang.org/x/term v0.28.0
