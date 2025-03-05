c17447ff592c2892c1b8f263601ec2ccfeb9c64e
Rusty Bee <145002912+rustybee42@users.noreply.github.com>
2025-03-05 09:23:15 +0100

    fix: align all columns left
    This looks much better and more consistent than some random right
    aligned cells (sometimes even if the cell content isn't a number)

--- ctl/internal/cmdfmt/fmt.go
+++ ctl/internal/cmdfmt/fmt.go
@@ -162,7 +162,7 @@ func (p *Printomatic) replacePrinter() {
 		// The column number is used here because Name does not work if there is no header (as is
 		// the case when pageSize=0). The ColumnConfig also recommends using this instead of name.
 		// The name is still included here because it is required when printing JSON.
-		colCfg = append(colCfg, table.ColumnConfig{Number: i + 1, Hidden: true, Name: name})
+		colCfg = append(colCfg, table.ColumnConfig{Number: i + 1, Hidden: true, Name: name, Align: text.AlignLeft, AlignHeader: text.AlignLeft})
 		for _, cName := range p.printCols {
 			if cName == name || cName == "all" {
 				colCfg[len(colCfg)-1].Hidden = false
